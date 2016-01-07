58fca04ea818ba2b50b8508a43ee11bcd7ec36f1
Andreas Niedermair <andreas@niedermair.name>
2016-01-07 16:09:39 +0100

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# SharpChangeLog
+
+This project can be used to generate a *change log*, by diff'ing the previous release-branch with the trunk that becomes the next release-branch. The extracted commits are parsed for Redmine issues, which are then fetched from a Redmine instance and outputted.
+
+Bare in mind that this is a quick'n'dirty project, so no fail-safe parsing of parameters and alike is done.
+
+## Example usage
+
+
+    SharpChangeLog.exe --repository=http://SVNSERVER/svn/REPOSITORY/ --trunk=trunk --branch=branch/release_XY --redmineHost=http://SERVER/redmine --redmineApiKey=????????????????????????????????????????
+
+## License
+
+SharpChangeLog is published under WTFNMFPL-1.0
+
+## Utilities
+
+- [morelinq](https://github.com/morelinq/MoreLINQ)
+- [NDesk.Options](http://www.ndesk.org/Options)
+- [redmine-api](https://github.com/zapadi/redmine-net-api)
+- [SharpSvn](https://sharpsvn.open.collab.net/)
---
+++ SharpChangeLog/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text.RegularExpressions;
+using MoreLinq;
+using NDesk.Options.Extensions;
+using Redmine.Net.Api;
+using Redmine.Net.Api.Types;
+using SharpSvn;
+
+// ReSharper disable ExceptionNotDocumentedOptional
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable ExceptionNotDocumented
+
+namespace SharpChangeLog
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO this code is prone to invalid inputs... idc :bikini:
+
+            var requiredValuesOptionSet = new RequiredValuesOptionSet();
+            var repository = requiredValuesOptionSet.AddRequiredVariable<string>("repository",
+                                                                                 "Specifies the URI to the SVN repository");
+            var trunk = requiredValuesOptionSet.AddRequiredVariable<string>("trunk",
+                                                                            "Specifies the relative path to the trunk, which is diffed against the <branch> parameter (usually trunk)");
+            var branch = requiredValuesOptionSet.AddRequiredVariable<string>("branch",
+                                                                             "Specifies the relative path to the branch, whose initial revision is used as the offset for the change log");
+            var redmineHost = requiredValuesOptionSet.AddRequiredVariable<string>("redmineHost",
+                                                                                  "Specifies the host, which serves the Redmine instance");
+            var redmineApiKey = requiredValuesOptionSet.AddRequiredVariable<string>("redmineApiKey",
+                                                                                    "Specifies the api key that is used for communication with the Redmine instance");
+            var consoleManager = new ConsoleManager("ChangeLog Generator",
+                                                    requiredValuesOptionSet);
+            if (!consoleManager.TryParseOrShowHelp(Console.Out,
+                                                   args))
+            {
+                return;
+            }
+
+            Console.WriteLine("Getting the initial revision of {0}",
+                              branch.Value);
+            var initialChangeItem = Program.GetInitialChangeItem(repository,
+                                                                 branch);
+            if (initialChangeItem == null)
+            {
+                Console.WriteLine("Could not get the initial revision of {0}",
+                                  branch.Value);
+                return;
+            }
+
+            Console.WriteLine("Getting log for {0} since {1}",
+                              trunk.Value,
+                              initialChangeItem.CopyFromRevision);
+            var logItems = Program.GetLogSince(repository,
+                                               trunk,
+                                               initialChangeItem.CopyFromRevision);
+
+            var issueIds = logItems.SelectMany(arg => arg.GetIssueIds())
+                                   .Distinct()
+                                   .Select(arg => arg.ToString())
+                                   .ToArray();
+
+            Console.WriteLine("Getting the issues for {0} since {1} from {2}",
+                              trunk.Value,
+                              initialChangeItem.CopyFromRevision,
+                              redmineHost.Value);
+            var issues = Program.GetIssues(redmineHost,
+                                           redmineApiKey,
+                                           issueIds);
+
+            issues.ForEach(issue =>
+                           {
+                               Console.WriteLine("#{0}: {1}",
+                                                 issue.Id,
+                                                 issue.Subject);
+                           });
+        }
+
+        public static SvnChangeItem GetInitialChangeItem(string repositoryUri,
+                                                         string target)
+        {
+            var svnLogArgs = new SvnLogArgs
+                             {
+                                 StrictNodeHistory = true,
+                                 BaseUri = new Uri(repositoryUri)
+                             };
+
+            Collection<SvnLogEventArgs> logItems;
+            using (var svnClient = new SvnClient())
+            {
+                if (!svnClient.GetLog(new Uri(svnLogArgs.BaseUri,
+                                              target),
+                                      svnLogArgs,
+                                      out logItems))
+                {
+                    logItems = new Collection<SvnLogEventArgs>();
+                }
+            }
+
+            var svnChangeItem = logItems.OrderBy(arg => arg.Revision)
+                                        .FirstOrDefault()
+                ?.ChangedPaths.FirstOrDefault();
+
+            return svnChangeItem;
+        }
+
+        public static ICollection<SvnLogEventArgs> GetLogSince(string repositoryUri,
+                                                               string target,
+                                                               long revision)
+        {
+            var svnLogArgs = new SvnLogArgs
+                             {
+                                 StrictNodeHistory = true,
+                                 End = revision,
+                                 BaseUri = new Uri(repositoryUri)
+                             };
+
+            Collection<SvnLogEventArgs> logItems;
+            using (var svnClient = new SvnClient())
+            {
+                if (!svnClient.GetLog(new Uri(svnLogArgs.BaseUri,
+                                              target),
+                                      svnLogArgs,
+                                      out logItems))
+                {
+                    logItems = new Collection<SvnLogEventArgs>();
+                }
+            }
+
+            return logItems;
+        }
+
+        public static IEnumerable<Issue> GetIssues(string redmineHost,
+                                                   string redmineApiKey,
+                                                   IEnumerable<string> issueIds)
+        {
+            var redmineManager = new RedmineManager(redmineHost,
+                                                    redmineApiKey,
+                                                    MimeFormat.json);
+            var result = issueIds.AsParallel() // we are doing a .AsParallel here, as the Redmine API is very slow :confused:
+                                 .Select(arg => redmineManager.GetObject<Issue>(arg,
+                                                                                new NameValueCollection()))
+                                 .ToArray();
+
+            return result;
+        }
+
+        public static IEnumerable<long> GetIssueIds(this SvnLogEventArgs svnLogEventArgs)
+        {
+            var result = Regex.Matches(svnLogEventArgs.LogMessage,
+                                       @"#(\d+)",
+                                       RegexOptions.Compiled)
+                              .Cast<Match>()
+                              .Select(match => long.Parse(match.Groups.Cast<System.Text.RegularExpressions.Group>()
+                                                               .ElementAt(1)
+                                                               .Value))
+                              .ToArray();
+
+            return result;
+        }
+    }
+}
